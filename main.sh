b09ca6d5b2b2867f30c9814654677aa4a5c967ad
Dmitriy Vinokurov <gim6626@gmail.com>
2019-06-03 21:20:28 +0500

    Added EDITOR environment variable

--- main.sh
+++ main.sh
@@ -6,6 +6,7 @@ alias grep='grep --color'
 alias less='less -RN'
 alias mc='mc --skin=modarin256'
 alias mcedit='mcedit --skin=modarin256'
+export EDITOR="mcedit --skin=modarin256"
 bind '"\e[A": history-search-backward'
 bind '"\e[B": history-search-forward'
 export HISTSIZE=10000
