acaead0f22960585e8ade420ac4e36dbfc4d1f2b
Дмитрий Винокуров <VinokurovDA@morion.ru>
2019-11-28 11:25:54 +0500

    Added skin configuration for mcview

--- main.sh
+++ main.sh
@@ -8,6 +8,7 @@ alias grep='grep --color'
 alias less='less -RN'
 alias mc='mc --skin=modarin256'
 alias mcedit='mcedit --skin=modarin256'
+alias mcview='mcview --skin=modarin256'
 export EDITOR="mcedit --skin=modarin256"
 bind '"\e[A": history-search-backward'
 bind '"\e[B": history-search-forward'
